3fbc33f1b5c2d6bd39854b8bb7c52701abe6e519
pembattech <pembat.tech@gmail.com>
2024-02-20 14:46:01 +0545

    Bug fix.

--- book_a_room/settings.py
+++ book_a_room/settings.py
@@ -140,8 +140,7 @@ STATIC_URL = '/static/'
 if DEBUG == True:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-elif DEBUG == False:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
